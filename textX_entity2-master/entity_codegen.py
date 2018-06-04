27d7bb553fc37c74d861c7d162577482ed2826c0
ndmc98 <norbey.munoz98@gmail.com>
2018-06-04 17:42:02 -0500

    Update entity_codegen.py

--- textX_entity2-master/entity_codegen.py
+++ textX_entity2-master/entity_codegen.py
@@ -15,7 +15,7 @@ def main(debug=False):
     entity_mm = get_entity_mm(debug)
 
     # Build Person model from person.ent file
-    person_model = entity_mm.model_from_file(join(this_folder, 'PROGRAMACION.ent'))
+    person_model = entity_mm.model_from_file(join(this_folder, 'SQL.ent'))
 
     def is_entity(n):
         """
@@ -43,13 +43,13 @@ def main(debug=False):
     jinja_env.tests['entity'] = is_entity
 
     # Load template
-    template0 = jinja_env.get_template('sql.template')
+    template0 = jinja_env.get_template('PROGRAMACION.template')
     template1 = jinja_env.get_template('ESTADIO.template')
     template2 = jinja_env.get_template('GRUPO.template')
     template3 = jinja_env.get_template('EQUIPO.template')
     
     for entity in person_model.entities:
-        # For each entity generate java file
+    
         if entity.name == 'PROGRAMACION':
             with open(join(srcgen_folder,
                        "%s.sql" % entity.name.capitalize()), 'w') as f:
